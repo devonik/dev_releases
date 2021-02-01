4013a8747dfd3d26adeb3ea280aafd36ee83334c
Niklas Grieger <niklas.grieger1@gmail.com>
2021-02-01 14:49:13 +0100

    update to latest flutter

--- android/app/src/main/java/devnik/dev_releases/Application.java
+++ android/app/src/main/java/devnik/dev_releases/Application.java
@@ -1,11 +1,11 @@
 package devnik.dev_releases;
 
 import io.flutter.app.FlutterApplication;
-import io.flutter.app.FlutterApplication;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin;
 
 public class Application extends FlutterApplication implements PluginRegistrantCallback {
     @Override
@@ -16,6 +16,6 @@ public class Application extends FlutterApplication implements PluginRegistrantC
 
     @Override
     public void registerWith(PluginRegistry registry) {
-        GeneratedPluginRegistrant.registerWith(registry);
+        FirebaseMessagingPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
     }
 }
